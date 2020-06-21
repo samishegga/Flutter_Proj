ab2f965ed54d3be153ac869f054ba87439db1d95
samishegga <47670429+samishegga@users.noreply.github.com>
2020-06-21 12:00:56 +0300

    three classes added to the main class

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyApp(),
+    home: Home(),
   ));
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
 }
